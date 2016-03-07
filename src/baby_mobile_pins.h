0c179fbf9da5b05581bec49cd31e4d29e0c878c7
Dmitry Rockosov <rockosov@gmail.com>
2016-03-07 21:43:05 +0300

    Commit final changes after assembling

--- src/baby_mobile_pins.h
+++ src/baby_mobile_pins.h
@@ -1,16 +1,16 @@
 #ifndef __BABY_MOBILE_PINS_H__
 #define __BABY_MOBILE_PINS_H__
 
-#define IR_REMOTE_PIN           8
-#define DF_MINI_PLAYER_RX_PIN   12
-#define DF_MINI_PLAYER_TX_PIN   13
+#define IR_REMOTE_PIN           2
+#define DF_MINI_PLAYER_RX_PIN   13
+#define DF_MINI_PLAYER_TX_PIN   12
 #define MOTOR_DIRECTION_PIN     4
 #define MOTOR_SPEED_PIN         5
-#define RGB_LED_RED_PIN         9
-#define RGB_LED_GREEN_PIN       10
+#define RGB_LED_RED_PIN         10
+#define RGB_LED_GREEN_PIN       9
 #define RGB_LED_BLUE_PIN        6
-#define DF_MINI_PLAYER_BUSY_PIN 2
-#define DF_MINI_PLAYER_BUSY_INT 0
+#define DF_MINI_PLAYER_BUSY_PIN 3
+#define DF_MINI_PLAYER_BUSY_INT 1
 
 #endif /* !__BABY_MOBILE_PINS_H__ */
 
